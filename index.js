5190abf1e4d3b49f3cc437d6024a4c56521b4dd5
Jeffrey <jeffrey.liu90@gmail.com>
2020-02-14 02:48:29 +0000

    ip address and coordinates for Space spotter

--- index.js
+++ index.js
@@ -1,9 +1,20 @@
-const { fetchMyIP } = require('./iss');
 
-fetchMyIP((error, ip) => {
+const { fetchMyIP } = require('./iss');
+const { fetchCoordsByIP } = require('./iss');
+
+
+// fetchMyIP((error, ip) => {
+//   if (error) {
+//     console.log("It didn't work!" , error);
+//     return;
+//   }
+//   console.log('It worked! Returned IP:' , ip);
+// });
+
+fetchCoordsByIP('162.245.144.188', (error, coords) => {
   if (error) {
     console.log("It didn't work!" , error);
     return;
   }
-  console.log('It worked! Returned IP:' , ip);
+  console.log('It worked! Returned Coords:' , coords);
 });
--- iss.js
+++ iss.js
@@ -2,19 +2,21 @@ const request = require('request');
 
 
 
-
 const fetchMyIP = function (callback) {
   request("https://api.ipify.org?format=json", (error, response, body) => {
     if (error) {
       return callback(error, null);
 
     }
+
+
     // if non-200 status, assume server error
     if (response.statusCode !== 200) {
       const msg = `Status Code ${response.statusCode} when fetching IP. Response: ${body}`
       callback(Error(msg), null)
       return;
     }
+    
     const ip = JSON.parse(body).ip
     callback(null, ip)
     console.log(ip)
@@ -25,16 +27,21 @@ const fetchMyIP = function (callback) {
 
 }
 
-module.exports = { fetchMyIP };
+const fetchCoordsByIP = function(ip, callback) {
+  request(`https://ipvigilante.com/json/${ip}`, (error, response, body) => {
+    if (error) {
+      callback(error, null);
+      return;
+    }
+    if (response.statusCode !== 200) {
+      callback(Error(`Status Code ${response.statusCode} when fetching Coordinates for IP: ${body}`), null);
+      return;
+    }
+    const latitude = JSON.parse(body).data.latitude;
+    const longitude = JSON.parse(body).data.longitude;
+    callback(null, { latitude, longitude });
+  });
+};
+module.exports = { fetchCoordsByIP };
+// module.exports = { fetchMyIP };
 
-// request("https://api.ipify.org?format=json", (error, response, body) => { // go to that url, end of url which is 'q=' you add sibe which will go to the page with Siberian cat info
-// // if (error) return callback(error, null); // if something goes wrong, it becomes error i.e. url is broken
-// // const data = JSON.parse(body); // parse the data into object
-// // if (data.length === 0) { // if the cat doesn't exist, create a call back labeled (erro,null, because in index.js theres two parameters, so if it is error, it will become error, and desc becomes null). it returns empty array if its wrong cat name
-// //   error = "Error, cat not found";
-//   callback(error, null);
-// } else {
-//   callback(null, data[0].description.trim()); // if cat is found, error becomes null and description is cat's description
-// }
-// });
-// };
